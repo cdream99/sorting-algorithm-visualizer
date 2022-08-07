321c619e3b659e89fc87703141ef5b5495a83511
cdream99 <krishnachaithanya826@gmail.com>
2022-08-07 18:24:58 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,69 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from "react";
+import Bar from "./Bar";
+import { bubbleSort } from "./bubbleSort";
+
 
 function App() {
+  const [numbers, setNumbers] = useState([]);
+  const [idx, setIDX] = useState({});
+  const [speed, setSpeed] = useState(10);
+
+  useEffect(() => {
+    generateRandomArray();
+  }, [])
+
+
+
+
+
+
+  const generateRandomArray = () => {
+    let array = [];
+    for (let i = 0; i < 200; i++) {
+      let x = Math.floor((Math.random() * 100) + 1);
+      array.push(x);
+    }
+    setNumbers(array);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className='row margin'>
+        <button className="button-margin" onClick={generateRandomArray}>Generate New Array</button>
+        <label for="speed">Speed</label>
+        <select name="speed" onChange={(e) => {
+          setSpeed(e.target.value);
+        }}>
+          <option value={10}>10ms</option>
+          <option value={50}>50ms</option>
+          <option value={250}>250ms</option>
+          <option value={500}>500ms</option>
+          <option value={1000}>1s</option>
+
+
+
+        </select>
+        <button className="button-margin" onClick={() => {
+          bubbleSort(numbers, setNumbers, setIDX, speed)
+        }}>Bubble Sort</button>
+      </div>
+      <div className='row'>
+
+        {
+          numbers?.map((item, index) => {
+            if (index == idx?.first || index == idx?.second) {
+              return <Bar item={item} color={"blue"}></Bar>
+            }
+            else {
+              return <Bar item={item} color={"lightcoral"} ></Bar>
+            }
+
+          })
+        }
+      </div>
+    </>
+
   );
 }
 
---
+++ src/Bar.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+
+export default function Bar({ item, color }) {
+
+    return <div className="bar-strip" style={{ height: `${item * 8}px`, backgroundColor: `${color}` }} ></ div>
+}
